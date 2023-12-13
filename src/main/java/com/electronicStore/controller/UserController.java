769686504f0db7883e189a4399753d8a04c0cbee
chaitanya-dange <semicd99@gmail.com>
2023-12-13 07:58:19 +0530

    api for uploading the image ...

--- src/main/java/com/electronicStore/controller/UserController.java
+++ src/main/java/com/electronicStore/controller/UserController.java
@@ -1,11 +1,18 @@
 package com.electronicStore.controller;
 
+import com.electronicStore.dtos.ImageResponse;
 import com.electronicStore.dtos.PageableResponse;
 import com.electronicStore.dtos.UserDto;
+import com.electronicStore.services.FileService;
 import com.electronicStore.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -13,6 +20,11 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private FileService fileService;
+
+    @Value("${user.profile.image.path}")
+    private  String imageUploadPath;
 
     //create
     @PostMapping()
@@ -65,4 +77,19 @@ public class UserController {
         List<UserDto> userDtos = userService.searchBy(keyword);
         return  userDtos;
     }
+
+    // upload user image
+
+    @PostMapping("/image/{userId}")
+    public ResponseEntity<ImageResponse> uploadUserImage(@RequestParam("userImage") MultipartFile image,@PathVariable String userId) throws IOException {
+        String imageName = fileService.uploadFile(image, imageUploadPath);
+        ImageResponse imageResp = ImageResponse.builder().imageName(imageName).status(HttpStatus.CREATED).success(true).build();
+        UserDto user = userService.getUserById(userId);
+        user.setImageName(imageName);
+        userService.updateUser(user,userId);
+
+        return  new ResponseEntity<>(imageResp,HttpStatus.CREATED);
+    }
+
+    // serve user image
 }
