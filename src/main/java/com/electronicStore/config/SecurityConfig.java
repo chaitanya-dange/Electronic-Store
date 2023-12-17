e83926b1325e316231c3df31624738eacb23e8ab
chaitanya-dange <semicd99@gmail.com>
2023-12-17 23:40:44 +0530

    Authentication using Basic Authentication , few methods are depricated  ,need further work.

--- src/main/java/com/electronicStore/config/SecurityConfig.java
+++ src/main/java/com/electronicStore/config/SecurityConfig.java
@@ -4,14 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Autowired
@@ -37,6 +42,30 @@ public class SecurityConfig {
 //        return new InMemoryUserDetailsManager(normalUser,adminUser);
 //
 //    }
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.csrf(csrf -> csrf.disable())
+//                .authorizeRequests(authorize -> authorize
+//                        .anyRequest().authenticated()
+//                )
+//                .httpBasic(Customizer.withDefaults())
+//                .cors(Customizer.withDefaults());
+
+        http
+                .csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+//        http.authorizeRequests()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .httpBasic();
+
+            return  http.build();
+    }
 
     @Bean
     public DaoAuthenticationProvider daoAuthenticationProvider(){
